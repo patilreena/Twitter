badee4b836d372b982b1e888a594508b4fe28224
Reena Patil <reena.patil@outlook.com>
2017-09-25 16:02:26 +1000

    added gem in gemfile

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -28,7 +28,6 @@ class TweetsController < ApplicationController
 
     respond_to do |format|
       if @tweet.save
-        postdate.strftime("%B %d, %Y");
         format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
         format.json { render :show, status: :created, location: @tweet }
       else
